370c4e3577c05c57a14eaf09cd48a187d236e65f
Flemming <fleeri91@gmail.com>
2025-05-29 01:09:43 +0200

    search page

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,9 +1,59 @@
-import { Text, View } from 'react-native'
+import { WaterTypeId } from '@/types/BathingWater/WaterType'
+import { useState } from 'react'
+import {
+  Keyboard,
+  SafeAreaView,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native'
+import { SegmentedButtons } from 'react-native-paper'
+import Autocomplete from '../components/BathingWaterSearch'
 
 export default function SearchView() {
+  const [segment, setSegment] = useState<string>('sea')
+
   return (
-    <View>
-      <Text>Search View</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <SafeAreaView style={styles.container}>
+        <SegmentedButtons
+          value={segment}
+          onValueChange={setSegment}
+          style={styles.segmentedButtons}
+          buttons={[
+            {
+              value: 'sea',
+              label: 'Hav',
+            },
+            {
+              value: 'lake',
+              label: 'Sjö',
+            },
+          ]}
+        />
+
+        <View style={styles.content}>
+          <Autocomplete
+            waterType={segment === 'sea' ? WaterTypeId.HAV : WaterTypeId.SJÖ}
+          />
+        </View>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0a0a0a',
+  },
+  segmentedButtons: {
+    padding: 24,
+  },
+  content: {
+    flex: 1,
+    width: '100%',
+    padding: 24,
+    alignItems: 'center',
+  },
+})
