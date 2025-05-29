ffd7bb4335fde81b3aef1a7fe29ae65a1e56ed95
Flemming <fleeri91@gmail.com>
2025-05-29 23:47:05 +0200

    bathing water profile query

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,15 +1,25 @@
 import { useQuery } from '@tanstack/react-query'
 
-import { getBathingWaters } from './api'
+import { getBathingWaterProfile, getBathingWaters } from './api'
 
 export const queryKeys = {
   bathingWaters: ['bathingWaters'],
+  bathingWaterProfile: ['bathingWaterProfile'],
 }
 
 export const useBathingWaters = () => {
   return useQuery({
     queryKey: queryKeys.bathingWaters,
     queryFn: getBathingWaters,
+    staleTime: 1000 * 60 * 60 * 24,
+    refetchInterval: 1000 * 30,
+  })
+}
+
+export const useBathingWaterProfile = (id: string) => {
+  return useQuery({
+    queryKey: queryKeys.bathingWaterProfile,
+    queryFn: () => getBathingWaterProfile(id),
     staleTime: 1000 * 60 * 60 * 24,
     refetchInterval: 1000 * 30,
   })
