926e9281d625235dfacb6271b46fb3cb6d52bd1d
Flemming <fleeri91@gmail.com>
2025-05-31 18:10:00 +0200

    update

--- store/useMapFilter.ts
+++ store/useMapFilter.ts
@@ -1,30 +1,27 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
 import { create } from 'zustand'
-import { createJSONStorage, persist } from 'zustand/middleware'
 
 import { MunicipalityName } from '@/constants/municipalities'
+import { BathingWater } from '@/types/BathingWater/BathingWaters'
 
 type MapFilterState = {
   municipality: MunicipalityName | null
+  selectedLocation: BathingWater | null
 }
 
 type MapFilterActions = {
-  setMunicipality: (value: MunicipalityName) => void
+  setMunicipality: (value: MunicipalityName | null) => void
+  setSelectedLocation: (value: BathingWater | null) => void
 }
 
 const initialState: MapFilterState = {
-  municipality: null,
+  municipality: 'Karlskrona',
+  selectedLocation: null,
 }
 
-export const useMapFilterStore = create<MapFilterState & MapFilterActions>()(
-  persist(
-    (set) => ({
-      ...initialState,
-      setMunicipality: (value) => set({ municipality: value }),
-    }),
-    {
-      name: 'map-filter-storage',
-      storage: createJSONStorage(() => AsyncStorage),
-    }
-  )
+export const useMapFilterStore = create<MapFilterState & MapFilterActions>(
+  (set) => ({
+    ...initialState,
+    setMunicipality: (value) => set({ municipality: value }),
+    setSelectedLocation: (value) => set({ selectedLocation: value }),
+  })
 )
