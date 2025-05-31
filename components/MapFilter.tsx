d17322991e913005ac65105980865379d23afb2a
Flemming <fleeri91@gmail.com>
2025-05-31 18:10:14 +0200

    map filter

--- components/MapFilter.tsx
+++ components/MapFilter.tsx
@@ -1,78 +1,33 @@
-import { municipalities, MunicipalityName } from '@/constants/municipalities'
-import { useMapFilterStore } from '@/store/useMapFilter'
-import { useMemo, useState } from 'react'
-import { ScrollView, StyleSheet, TouchableOpacity, View } from 'react-native'
-import { List, TextInput, useTheme } from 'react-native-paper'
+import { useRef } from 'react'
+import { StyleSheet, View } from 'react-native'
+import ActionSheet, { ActionSheetRef } from 'react-native-actions-sheet'
+import { useTheme } from 'react-native-paper'
+
+import MunicipalitySearch from './MunicipalitySearch'
 
 export default function MapFilter() {
-  const { municipality, setMunicipality } = useMapFilterStore()
-  const [query, setQuery] = useState('')
-  const { colors } = useTheme()
-
-  const filteredMunicipalities = useMemo(() => {
-    if (!query) return []
-    const lower = query.toLowerCase()
-    return municipalities
-      .filter((name) => name.toLowerCase().startsWith(lower))
-      .sort((a, b) => a.localeCompare(b)) // Sort ascending alphabetically
-      .slice(0, 20)
-  }, [query])
-
-  const handleSelect = (name: MunicipalityName) => {
-    setMunicipality(name)
-    setQuery(name)
-  }
+  const actionSheetRef = useRef<ActionSheetRef>(null)
+  const theme = useTheme()
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        label="Välj kommun"
-        placeholder="Sök kommun"
-        value={query}
-        onChangeText={setQuery}
-        mode="outlined"
-        style={styles.input}
-      />
-
-      {filteredMunicipalities.length > 0 && (
-        <ScrollView
-          style={styles.listContainer}
-          keyboardShouldPersistTaps="handled"
-        >
-          <List.Section style={styles.list}>
-            {filteredMunicipalities.map((name) => (
-              <TouchableOpacity
-                key={name}
-                onPress={() => handleSelect(name as MunicipalityName)}
-              >
-                <List.Item
-                  title={name}
-                  titleStyle={{ color: colors.onSurface }}
-                />
-              </TouchableOpacity>
-            ))}
-          </List.Section>
-        </ScrollView>
-      )}
-    </View>
+    <ActionSheet
+      ref={actionSheetRef}
+      gestureEnabled={true}
+      indicatorStyle={{
+        width: 100,
+      }}
+      snapPoints={[90]}
+    >
+      <View style={styles.actionSheetcontainer}>
+        <MunicipalitySearch />
+      </View>
+    </ActionSheet>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  actionSheetcontainer: {
     width: '100%',
-    marginTop: 20,
-  },
-  input: {
-    marginBottom: 5,
-  },
-  list: {
-    elevation: 2,
-    borderRadius: 4,
-  },
-  listContainer: {
-    maxHeight: 200,
-    borderRadius: 4,
-    elevation: 2,
+    height: '100%',
   },
 })
