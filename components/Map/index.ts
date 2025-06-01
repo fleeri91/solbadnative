572b1e1d7f970d18c92d82a70c1b77d2e14f85fc
Flemming <fleeri91@gmail.com>
2025-06-01 20:58:10 +0200

    removed component

--- components/Map/index.ts
+++ components/Map/index.ts
@@ -1,6 +1,5 @@
 import BathingWaterMarker from './BathingWaterMarker'
 import DropMarker from './DropMarker'
 import Map from './Map'
-import UserLocationMarker from './UserLocationMarker'
 
-export { BathingWaterMarker, DropMarker, Map, UserLocationMarker }
+export { BathingWaterMarker, DropMarker, Map }
