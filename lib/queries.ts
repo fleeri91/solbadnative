4b3cf4f2b20fcc2cc535de1cf1e1ff4a1a740eb4
Flemming <fleeri91@gmail.com>
2025-06-01 23:38:35 +0200

    removed stale

--- lib/queries.ts
+++ lib/queries.ts
@@ -20,7 +20,5 @@ export const useBathingWaterProfile = (id: string) => {
   return useQuery({
     queryKey: queryKeys.bathingWaterProfile,
     queryFn: () => getBathingWaterProfile(id),
-    staleTime: 1000 * 60 * 60 * 24,
-    refetchInterval: 1000 * 30,
   })
 }
