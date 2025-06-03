034a54cf3eee5bc77983e226f69a6bd545d380c7
Flemming <fleeri91@gmail.com>
2025-06-03 23:53:22 +0200

    color and zoom functionality

--- components/Map/BathingWaterMarker.tsx
+++ components/Map/BathingWaterMarker.tsx
@@ -1,27 +1,84 @@
 // components/BathingWaterMarker.tsx
 import { BathingWater } from '@/types/BathingWater/BathingWaters'
-import MaterialIcons from '@expo/vector-icons/MaterialIcons'
+import { WaterTypeId } from '@/types/BathingWater/WaterType'
+import { StyleSheet, View } from 'react-native'
 import { Marker } from 'react-native-maps'
 
 type Props = {
   water: BathingWater
-  onPress: (water: BathingWater) => void
+  zoomLevel: number
 }
 
-export default function BathingWaterMarker({ water, onPress }: Props) {
+export default function BathingWaterMarker({ water, zoomLevel }: Props) {
   const latitude = parseFloat(water.samplingPointPosition.latitude)
   const longitude = parseFloat(water.samplingPointPosition.longitude)
 
   if (isNaN(latitude) || isNaN(longitude)) return null
 
+  const size = Math.max(16, Math.min(40, zoomLevel * 2))
+
+  const getColorByWaterType = (waterType: WaterTypeId) => {
+    switch (waterType) {
+      case 1: // Sea
+        return '#007AFF'
+      case 3: // Lake
+        return '#4ABDAC'
+      default:
+        return '#FF3B30'
+    }
+  }
+
+  const onSelect = (water: BathingWater) => {
+    if (zoomLevel > 11) {
+      console.log(water.name)
+    }
+  }
+
   return (
     <Marker
       coordinate={{ latitude, longitude }}
-      title={water.name}
-      description={water.description || ''}
-      onPress={() => onPress(water)}
+      onSelect={() => onSelect(water)}
     >
-      <MaterialIcons name="location-pin" size={36} color="tomato" />
+      <View style={styles.container}>
+        <View
+          style={[
+            styles.markerOuter,
+            {
+              width: size,
+              height: size,
+              borderRadius: size / 2,
+            },
+          ]}
+        >
+          <View
+            style={[
+              styles.markerInner,
+              {
+                width: size * 0.6,
+                height: size * 0.6,
+                borderRadius: (size * 0.6) / 2,
+                backgroundColor: getColorByWaterType(water.waterTypeId),
+              },
+            ]}
+          />
+        </View>
+      </View>
     </Marker>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  markerOuter: {
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'rgba(0,0,0,0.1)',
+    borderWidth: 1,
+  },
+  markerInner: {
+    backgroundColor: 'tomato', // dynamic in render
+  },
+})
